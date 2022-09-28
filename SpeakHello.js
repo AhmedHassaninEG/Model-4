a43259b2fba853f492a7249f346cf83f052840d8
AhmedHassaninEG <109860108+AhmedHassaninEG@users.noreply.github.com>
2022-09-28 23:03:52 +0200

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,5 +1,9 @@
 var speakWord = "Hello Mr Ramy";
 consol.loge=(speakWord);
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
+
 
 var greating = "Hello Mr Adam";
 console.log =(greaing);
